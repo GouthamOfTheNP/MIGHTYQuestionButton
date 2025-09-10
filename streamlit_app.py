f63129a5d694480018df4be775d9dfe39d59ec79
Goutham Pedinedi <107291366+GouthamOfTheNP@users.noreply.github.com>
2025-09-10 17:57:30 -0500

    Reorder topics in the selectbox options

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write("Press the MIGHTY Question Button to generate a challenging, thought-pr
 if "question" not in st.session_state:
 	st.session_state.question = None
 
-topics = ["AP Physics C", "AP Calculus BC", "SAT Math", "SAT Reading and Writing", "AP Spanish 4 MCQ Main Idea", "AP Spanish 4 MCQ Contextual", "Other"]
+topics = ["AP Spanish 4 MCQ Main Idea", "AP Spanish 4 MCQ Contextual", "AP Physics C", "AP Calculus BC", "SAT Math", "SAT Reading and Writing", "Other"]
 dropdown = st.selectbox("Select a topic", topics)
 
 custom_topic = st.text_input("Enter your custom topic:") if dropdown == "Other" else "AP Spanish 4 MCQ Questions in Spanish with source article in full-text format following AP Spanish Guidelines for the length, paragraph count, and complexity of the source article, with questions focusing on specific details rather than the main idea" if dropdown == "AP Spanish 4 MCQ Contextual" else "AP Spanish 4 MCQ Questions in Spanish with source article in full-text format following AP Spanish Guidelines for the length, paragraph count, and complexity of the source article, with questions focusing on main idea" if dropdown == "AP Spanish 4 MCQ Main Idea" else ""
